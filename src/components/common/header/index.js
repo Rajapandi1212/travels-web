ebde667b4307d1177e22c0485e2081b1ab25418d
krishnakumars59809 <116875316+krishnakumars59809@users.noreply.github.com>
2023-01-29 13:02:46 -0500

    Header UI update

--- src/components/common/header/index.js
+++ src/components/common/header/index.js
@@ -1,6 +1,6 @@
 import { AppBar, Avatar, Toolbar } from "@mui/material";
 import { Box } from "@mui/system";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "../../../styles/components/Header.css";
 import Logo from "../../../assets/images/logo/logo1.svg";
 const Header = () => {
@@ -9,29 +9,41 @@ const Header = () => {
     { item: "About", link: "/about" },
   ];
   const ActiveClass = {
-    color: "white",
     textDecoration: "none",
     fontSize: "large",
     letterSpacing: 3,
     fontFamily: " Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif",
+    backgroundImage: "linear-gradient(120deg, blue, blue, blue, blue,blue,blue,blue, green, green,green, green,green) ",
+    animation:" button-animation 0.65s 0.30s ease-out ",
+    backgroundSize: 100,
+    backgroundRepeat: "repeat",
+    webkitBackgroundClip: "text",
+    WebkitTextFillColor: "transparent",
+    mozBackgroundClip: "text",
+    mozTextFillColor: "transparent",
+    textDecoration:'underline',
+    borderBottom:" 2px solid blue"
+
   };
   const NonActiveClass = {
-    color: "#1e3f4f",
-    textDecoration: "none",
+    textDecoration: "none", 
     fontSize: "large",
     fontFamily: " Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif",
     letterSpacing: 3,
+    color:"black",
+
   };
   return (
     <Box>
       <AppBar
         position="fixed"
-        // sx={{
-        //   boxShadow: "0px 1px 2px 0px #d8fff3c2, 0px 1px 1px 0px #e5f2ee;",
-        //   zIndex: 3
-        // }}
-        className="header_container"
-      >
+        sx={{
+          boxShadow: "0px 1px 2px 0px #d8fff3c2, 0px 1px 1px 0px #e5f2ee;",
+          zIndex: 3,
+          bgcolor:"#f2f5f3"
+        }}
+
+>
         <Toolbar
           sx={{
             display: "flex",
@@ -42,8 +54,8 @@ const Header = () => {
             mx: "auto",
           }}
         >
-          <Box sx={{ display: "flex", gap: 3, alignItems: "center" }}>
-            <img
+          <Box sx={{ display: "flex", gap: 3, alignItems: "center" }} >
+           <Link to="/"><img
               src={Logo}
               style={{
                 height: "50px",
@@ -52,14 +64,16 @@ const Header = () => {
                 backgroundColor: "#32ce9c",
               }}
               alt="logo"
-            />
+             
+            /></Link> 
             {NAVITEM.map(({ item, link }, i) => (
               <NavLink
                 Key={i}
                 to={link}
                 style={({ isActive }) =>
-                  isActive ? ActiveClass : NonActiveClass
+                  isActive ? (ActiveClass) : (NonActiveClass)
                 }
+               
               >
                 {item}
               </NavLink>
